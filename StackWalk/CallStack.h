a2f0924db84995d4f5c5646903965c83031e6d47
Basudev Patel <1basudevpatel@gmail.com>
2020-04-29 14:30:30 +0530

    Test for call stacks from inside threads and also deeper callstacks

--- StackWalk/CallStack.h
+++ StackWalk/CallStack.h
@@ -17,22 +17,8 @@
 #pragma comment(lib, "psapi.lib")
 #pragma comment(lib, "dbghelp.lib")
 
-struct StackWalkContext
-{
-    static const auto k_FunctionNameLen = 63;
-    HANDLE Process;
-    HANDLE Thread;
-    STACKFRAME64 StackFrame;
-    CONTEXT ContextRecord;
-    DWORD64 Displacement;
-    IMAGEHLP_SYMBOL64 Symbol;
-    char FunctionName[k_FunctionNameLen];
-    DWORD Offset;
-    IMAGEHLP_LINE64 Line;
-};
-
 // This has to be a macro to capture context at the call site.
-#define StackWalk_Start(context__, process__)\
+#define CallStack_StartCapture(context__, process__)\
 {\
     context__ = { (HANDLE)process__, GetCurrentThread(), {}, {} };\
     context__.ContextRecord.ContextFlags = CONTEXT_FULL;\
@@ -44,14 +30,28 @@ struct StackWalkContext
     context__.StackFrame.AddrFrame.Offset = context__.ContextRecord.Rbp;\
     context__.StackFrame.AddrFrame.Mode   = AddrModeFlat;\
     context__.Symbol.SizeOfStruct  = sizeof(IMAGEHLP_SYMBOL64);\
-    context__.Symbol.MaxNameLength = StackWalkContext::k_FunctionNameLen;\
+    context__.Symbol.MaxNameLength = CallStackContext::k_FunctionNameLen;\
 }
 
 #if __cplusplus
 extern "C" {
 #endif
 
-uint64_t StackWalk_Init()
+typedef struct CallStackContext_
+{
+    static const auto k_FunctionNameLen = 63;
+    HANDLE Process;
+    HANDLE Thread;
+    STACKFRAME64 StackFrame;
+    CONTEXT ContextRecord;
+    DWORD64 Displacement;
+    IMAGEHLP_SYMBOL64 Symbol;
+    char FunctionName[k_FunctionNameLen];
+    DWORD Offset;
+    IMAGEHLP_LINE64 Line;
+} CallStackContext;
+
+uint64_t CallStack_Init()
 {
     auto process = GetCurrentProcess();
     SymInitialize(process, 0, true);
@@ -59,7 +59,7 @@ uint64_t StackWalk_Init()
     return (uint64_t)process;
 }
 
-bool StackWalk_Next(StackWalkContext* inOutContext)
+bool CallStack_Next(CallStackContext* inOutContext)
 { 
     auto result = StackWalk64(
         IMAGE_FILE_MACHINE_AMD64,
@@ -95,7 +95,7 @@ bool StackWalk_Next(StackWalkContext* inOutContext)
     return result;
 }
 
-void StackWalk_Shutdown(uint64_t process)
+void CallStack_Shutdown(uint64_t process)
 {
     SymCleanup((HANDLE)process);
 }
--- StackWalk/StackWalk.cpp
+++ StackWalk/StackWalk.cpp
@@ -1,37 +1,107 @@
 #include <stdio.h>
-#include "StackWalk.h"
+#include "CallStack.h"
 
-void PrintStack()
-{
-    auto process = StackWalk_Init();
+#include <thread>
+#include <mutex>
 
-    StackWalkContext context = { (HANDLE)process };
-    StackWalk_Start(context, process);
+void PrintStack();
 
-    while (StackWalk_Next(&context))
-    {
-        printf("0x%llx __ %s __ file %s, line %d\n", 
-            context.Symbol.Address,
-            context.FunctionName, 
-            context.Line.FileName, 
-            context.Line.LineNumber);
-    }
-
-    StackWalk_Shutdown(process);
-}
+#define FooMember(idx__, idxM1__)\
+    void Fn##idx__(uint64_t process) { Fn##idxM1__(process); }
 
 class Foo
 {
 public:
-    void Fn5() { PrintStack(); }
-    void Fn4() { Fn5(); }
-    void Fn3() { Fn4(); }
-    void Fn2() { Fn3(); }
-    void Fn1() { Fn2(); }
+    FooMember(16, 15);
+    FooMember(15, 14);
+    FooMember(14, 13);
+    FooMember(13, 12);
+    FooMember(12, 11);
+    FooMember(11, 10);
+    FooMember(10,  9);
+    FooMember( 9,  8);
+    FooMember( 8,  7);
+    FooMember( 7,  6);
+    FooMember( 6,  5);
+    FooMember( 5,  4);
+    FooMember( 4,  3);
+    FooMember( 3,  2);
+    FooMember( 2,  1);
+    void Fn1(uint64_t process) { PrintStack(process); }
 };
+
+void CallFoo(uint64_t process)
+{
+    Foo foo;
+    foo.Fn16(process);
+}
+
+std::mutex g_LogMutex;
+
+void TestThreads(int numThreads, uint64_t process)
+{
+    std::thread* threads = new std::thread[numThreads];
+
+    for (auto i = 0; i < numThreads; i++)
+        threads[i] = std::thread(CallFoo, process);
+
+    for (auto i = 0; i < numThreads; i++)
+        threads[i].join();
+}
 
 int main()
 {
-    Foo foo;
-    foo.Fn1();
+    auto process = CallStack_Init();
+
+    CallFoo(process);
+    TestThreads(4, process);
+
+    CallStack_Shutdown(process);
+}
+
+void PrintStack(uint64_t process)
+{
+    struct LineInfo
+    {
+        uint64_t Address;
+        char* FileName;
+        uint32_t LineNumber;
+        char FunctionName[63];
+    };
+
+    const int k_MaxCapturedLines = 24;
+    LineInfo lines[k_MaxCapturedLines];
+    size_t linesCount = 0;
+
+    CallStackContext context = { (HANDLE)process };
+    CallStack_StartCapture(context, process);
+
+    while (CallStack_Next(&context))
+    {
+        auto i = linesCount;
+        
+        lines[i].Address = context.Symbol.Address;
+        lines[i].FileName = context.Line.FileName;
+        lines[i].LineNumber = context.Line.LineNumber;
+        strcpy_s(lines[i].FunctionName, context.FunctionName);
+
+        linesCount++;
+        if (linesCount == k_MaxCapturedLines)
+            break;
+    }
+
+    g_LogMutex.lock();
+
+    printf("\n---- Thread: %d ----\n\n", std::this_thread::get_id());
+
+    for (auto i = 0; i < linesCount; i++)
+    {
+        printf("0x%llx __ %s __ file %s, line %d\n", 
+            lines[i].Address,
+            lines[i].FunctionName, 
+            lines[i].FileName, 
+            lines[i].LineNumber);
+    }
+
+    g_LogMutex.unlock();
 }
