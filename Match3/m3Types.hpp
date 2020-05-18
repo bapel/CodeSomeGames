699a1b636cbd6e4bdce40817699a69cd9e6f5c0f
Basudev Patel <1basudevpatel@gmail.com>
2020-05-19 01:39:37 +0530

    Structs to represent spans

--- Match3/m3Types.hpp
+++ Match3/m3Types.hpp
@@ -3,6 +3,7 @@
 #include <EASTL\array.h>
 #include <EASTL\type_traits.h>
 #include <EASTL\algorithm.h>
+#include <EASTL\tuple.h>
 
 #include <cstdint>
 #include <cassert>
@@ -11,10 +12,12 @@ namespace m3
 {
     using row_t = int8_t;
     using col_t = int8_t;
+
     using count_t = uint16_t;
 
     namespace private_
     {
+        // @Todo: Use a vector instead of an array so size can be set at runtime.
         template <class T, int R, int C>
         struct Board
         {
@@ -72,25 +75,81 @@ namespace m3
 
     const gem_color_t GemColors[] = { ' ', 'B', 'R', 'O', 'G', 'Y' };
 
-    const row_t BoardRows = 8;
-    const col_t BoardCols = 8;
-
     struct Gem
     {
         gem_id_t Id;
         gem_color_t Color;
     };
 
-    template <class T>
-    using Board = private_::Board<T, BoardRows, BoardCols>;
+    template <class T, class S>
+    struct Span
+    {
+        T m_T;
+        S m_0, m_1;
+
+        Span(T r, S s0, S s1) : 
+            m_T(r), m_0(s0), m_1(s1) 
+        { 
+            assert(m_0 <= m_1); 
+        }
+
+        inline S Count() const { return m_1 - m_1; }
+        inline bool Contains(S s) const { return s >= m_0 || s <= m_1; }
+    };
+
+    template <class T, class S>
+    Span<T, S> Union(const Span<T, S>& a, const Span<T, S>& b)
+    {
+        assert(a.m_0 == b.m_0);
+        assert(a.Contains(b.m_0) || a.Contains(b.m_1));
+        return { a.m_T, SDL_min(a.m_0, b.m_0), SDL_max(a.m_1, b.m_1) };
+    }
+
+    struct RowSpan : Span<row_t, col_t>
+    {
+        RowSpan(row_t r, col_t c0, col_t c1) : 
+            Span<row_t, col_t>(r, c0, c1)
+        { }
+
+        inline row_t Row() const { return m_T; }
+        inline col_t Col_0() const { return m_0; }
+        inline col_t Col_1() const { return m_1; }
+
+        inline eastl::tuple<row_t, col_t> operator[] (col_t c) const 
+        {
+            assert(c < Count());
+            return { Row(), Col_0() + c };
+        }
+    };
+
+    struct ColSpan : Span<col_t, row_t>
+    {
+        ColSpan(row_t r0, row_t r1, col_t c) : 
+            Span<col_t, row_t>(c, r0, r1)
+        { }
+
+        inline row_t Row_0() const { return m_0; }
+        inline col_t Row_1() const { return m_1; }
+        inline col_t Col() const { return m_T; }
+
+        inline eastl::tuple<row_t, col_t> operator[] (row_t r) const 
+        {
+            assert(r < Count());
+            return { Row_0() + r, Col() };
+        }
+    };
 
     struct Matches
     {
-        struct { row_t Row; col_t Col_0, Col_1; } Row_0;
-        struct { row_t Row; col_t Col_0, Col_1; } Row_1;
-        struct { col_t Col; row_t Row_0, Row_1; } Col_0;
-        struct { col_t Col; row_t Row_0, Row_1; } Col_1;
+        RowSpan Row_0, Row_1;
+        ColSpan Col_0, Col_1;
     };
+
+    const row_t BoardRows = 8;
+    const col_t BoardCols = 8;
+
+    template <class T>
+    using Board = private_::Board<T, BoardRows, BoardCols>;
 }
 
 #ifdef CatchAvailable__
